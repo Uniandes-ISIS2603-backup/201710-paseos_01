bf156cdf0a33db599fa4461fcc32a35d029200ff
jma.lovera10 <jma.lovera10@ISIS2603-0123.sis.virtual.uniandes.edu.co>
2017-03-10 07:34:37 -0500

    Creación persistence

---
+++ paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/persistence/OfertaPersistence.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.paseos.persistence;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jma.lovera10
+ */
+@Stateless
+public class OfertaPersistence {
+    
+    @PersistenceContext(unitName="paseosPU")
+    protected EntityManager em;
+}
