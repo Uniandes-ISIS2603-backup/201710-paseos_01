fd4f5e18693ab1269fb5bfc069e5a1a2128d5297
jma.lovera10 <jma.lovera10@ISIS2603-0123.sis.virtual.uniandes.edu.co>
2017-03-10 08:25:27 -0500

    Try FotoLogic

--- paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/ejbs/FotoLogic.java
+++ paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/ejbs/FotoLogic.java
@@ -5,7 +5,9 @@
  */
 package co.edu.uniandes.csw.paseos.ejbs;
 
+import co.edu.uniandes.csw.paseos.entities.FotoEntity;
 import co.edu.uniandes.csw.paseos.persistence.FotoPersistence;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -16,5 +18,64 @@ import javax.inject.Inject;
 @Stateless
 public class FotoLogic {
     
-    @Inject private FotoPersistence presistence;
+    @Inject private FotoPersistence persistence;
+    
+    /**
+     * Obtiene la lista de los registros de Foto.
+     *
+     * @return Colección de objetos de FotoEntity.
+     * 
+     */
+   
+    public List<FotoEntity> getFotos() {
+        return persistence.findAll();
+    }
+
+
+    /**
+     * Obtiene los datos de una instancia de Foto a partir de su ID.
+     *
+     * @param id Identificador de la instancia a consultar
+     * @return Instancia de FotoEntity con los datos de la Foto consultada.
+     *
+     */
+    public FotoEntity getFotos(Long id) {
+        return persistence.find(id);
+    }
+
+    /**
+     * Se encarga de crear una Foto en la base de datos.
+     *
+     * @param entity Objeto de FotoEntity con los datos nuevos
+     * @return Objeto de FotoEntity con los datos nuevos y su ID.
+     * @generated
+     */
+    
+    public FotoEntity createFoto(FotoEntity entity) {
+        persistence.create(entity);
+        return entity;
+    }
+
+    /**
+     * Actualiza la información de una instancia de Foto.
+     *
+     * @param entity Instancia de FotoEntity con los nuevos datos.
+     * @return Instancia de FotoEntity con los datos actualizados.
+     * 
+     */
+   
+    public FotoEntity updateFoto(FotoEntity entity) {
+        return persistence.update(entity);
+    }
+
+    /**
+     * Elimina una instancia de Foto de la base de datos.
+     *
+     * @param id Identificador de la instancia a eliminar.
+     *
+     */
+   
+    public void deleteFoto(Long id) {
+        persistence.delete(id);
+    }
 }
