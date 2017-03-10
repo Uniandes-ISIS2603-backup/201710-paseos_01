c8582845f6b96802ab9bf6cd32cf76e5bc4a5fbb
jma.lovera10 <jma.lovera10@ISIS2603-0123.sis.virtual.uniandes.edu.co>
2017-03-10 08:14:09 -0500

    Creación Persistence

---
+++ paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/ejbs/VisitaLogic.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.paseos.ejbs;
+
+import co.edu.uniandes.csw.paseos.persistence.VisitaPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author jma.lovera10
+ */
+@Stateless
+public class VisitaLogic {
+    
+    @Inject private VisitaPersistence presistence;
+}
