b893d497e6b80786920786f6c19db4ff2242cd7c
re.vega11 <re.vega11@ISIS2603-0115.sis.virtual.uniandes.edu.co>
2017-03-12 10:18:51 -0500

    Commit 1: Prueba

---
+++ paseos-01-web/src/main/java/co/edu/uniandes/csw/paseos/resources/PaseoResource.java
@@ -0,0 +1,48 @@
+
+package co.edu.uniandes.csw.paseos.resources;
+
+import co.edu.uniandes.csw.employee.dtos.HabiDTO;
+import co.edu.uniandes.csw.employee.ejbs.HabitacionLogic;
+import co.edu.uniandes.csw.employee.entities.HabitacionEntity;
+import co.edu.uniandes.csw.employee.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/habitaciones")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PaseoResource {
+    
+    @Inject
+    private HabitacionLogic habitacionLogic;
+    
+    //GET /habitaciones -- obtiene todas las Habitaciones
+    @GET
+    public List <HabitacionDTO> getCompanies(){
+        List <HabitacionDTO> habitacionDTOs = new ArrayList<>();
+        List <HabitacionEntity> habitaciones = habitacionLogic.getHabitaciones();
+        for(HabitacionEntity habitacion : habitaciones){
+            HabitacionDTO dto = new HabitacionDTO(habitacion);
+            habitacionDTOs.add(dto);
+        }
+        return habitacionDTOs;
+    }
+    
+    //POST /companies -- agrega una habitacion
+    @POST
+    public HabitacionDTO addCompany(HabitacionDTO habitacionDTO)throws BusinessLogicException{
+        HabitacionEntity habitacion = habitacionDTO.toEntity();
+        HabitacionEntity storedHabitacion = habitacionLogic.createHabitacion(habitacion);
+        return new HabitacionDTO(storedHabitacion);
+    }
+ }
