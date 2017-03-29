2f04ed26818be8efeccf5d00a604ec445fb6a170
jma.lovera10 <jma.lovera10@ISIS2603-0123.sis.virtual.uniandes.edu.co>
2017-03-29 08:49:54 -0500

    Update Bower components and path

--- paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/entities/FotoEntity.java
+++ paseos-01-logic/src/main/java/co/edu/uniandes/csw/paseos/entities/FotoEntity.java
@@ -1,9 +1,4 @@
-// TODO: eliminar los comentarios por defecto
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.edu.uniandes.csw.paseos.entities;
 
 import java.io.Serializable;
