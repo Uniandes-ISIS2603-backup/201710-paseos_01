c2137e285f3e0959dc5769c70a147958a73fc1fa
Tomas F. Venegas Bernal <tf.venegas10@uniandes.edu.co>
2017-03-30 12:30:47 -0500

    pasos catalogo y detail funcionan

--- paseos-01-web/src/main/webapp/src/modules/paseos/appPaseos.js
+++ paseos-01-web/src/main/webapp/src/modules/paseos/appPaseos.js
@@ -6,13 +6,12 @@ var appPaseos=ng.module('appPaseos',['ui.router']);
             // En basePath se encuentran los templates y controladores de módulo
             var basePath = 'src/modules/paseos/';
             // Mostrar la lista de libros será el estado por defecto del módulo
-            $urlRouterProvider.otherwise("/paseos");
-            // Definición del estado 'booksList' donde se listan los libros
-            $stateProvider.state('paseos', {
-                // Url que aparecerá en el browser
+            $urlRouterProvider.otherwise("paseos/catalogo");
+           //Definición de un estado abstracto
+           $stateProvider.state('paseos',{
+               
+                abstract:true,
                 url: '/paseos',
-                // Se define una variable books (del estado) que toma por valor 
-                // la colección de libros que obtiene utilizando $http.get 
                  resolve: {
                     getPaseos: ["$http",function($http ){
                     return $http.get("/paseos-01-web/api/paseos").success(function(data){
@@ -22,27 +21,46 @@ var appPaseos=ng.module('appPaseos',['ui.router']);
                     });
                }]
                 },
-                // Template que se utilizara para ejecutar el estado
-                templateUrl: basePath + 'paseosCatalogo.html',
-                // El controlador guarda en el scope en la variable booksRecords los datos que trajo el resolve
-                // booksRecords será visible en el template
-                controller: ['$scope','getPaseos', function($scope,getPaseos){
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'paseos.html',
+                         controller: ['$scope','getPaseos', function($scope,getPaseos){
           
                     $scope.catalogo=getPaseos.data;
        
         
-                }]            
-            });
+                }]  
+                    }
+                    
+                }
+               
+           })
             
-            $stateProvider.state('paseoDetail',{
-                url: '/{idPaseo}',
+            
+            // Definición del estado 
+           
+            .state('paseosCatalogo', {
+                // Url que aparecerá en el browser
+                url: '/catalogo',
+                parent:"paseos",
+                
+                
+                views: {
+                    'catalogoView': {
+                // Template que se utilizara para ejecutar el estado
+                templateUrl: basePath + 'paseosCatalogo.html',
+                // El controlador guarda en el scope en la variable booksRecords los datos que trajo el resolve
+                // booksRecords será visible en el template
+               }
+            }}).state('paseoDetail',{
+                url: '/{idPaseo: int}/detail',
                 parent:"paseos",
                 param : {
                     idPaseo: null
                 },
                 resolve: {
-                    getPaseo: ["$http",function($http ){
-                    return $http.get("/paseos-01-web/api/paseos/"+idPaseo).success(function(data){
+                    getPaseo: ["$http",'$stateParams', function($http, $params ){
+                    return $http.get("/paseos-01-web/api/paseos/"+ $params.idPaseo.toString()).success(function(data){
                         return data;
                     }).error(function(err){
                         return err;
@@ -50,13 +68,20 @@ var appPaseos=ng.module('appPaseos',['ui.router']);
                }]
                 },
                 views:{
-                     templateUrl: basePath + 'paseo.detail.html',
-                        controller: ['$scope','getPaseo', function ($scope, getPaseo) {
+                'catalogoView': {
+                // Template que se utilizara para ejecutar el estado
+                templateUrl: basePath + 'paseosCatalogo.html',
+             
+                 },
+                'detailView':{
+                templateUrl: basePath + 'paseo.detail.html',
+                controller: ['$scope','getPaseo', function ($scope, getPaseo) {
+                             //   $window.document.getElementById("view").ui-view="detailedView";
                                 $scope.paseoActual = getPaseo.data;
                             }]
                 
-                }
-            
+                
+                    }}
             });
         }
     ]);
