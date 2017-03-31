e23f9ecf8138eb91b962fe6f1c49c285f651d340
n.acevedos <n.acevedos@ISIS2603-0124.sis.virtual.uniandes.edu.co>
2017-03-31 15:49:12 -0500

    Usuarios ya salen: versión no detallada.

---
+++ paseos-01-web/src/main/webapp/src/modules/usuarios/usuarios.mod.js
@@ -0,0 +1,51 @@
+(function (ng) {
+    var mod = ng.module("usuariosModule", ['ui.router']);
+    mod.constant("usuariosContext", "api/usuarios");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/usuarios/';
+            $urlRouterProvider.otherwise("/usuariosList");
+
+            $stateProvider.state('usuarios', {
+                url: '/usuarios',
+                abstract: true,
+                resolve: {
+                    usuarios: ['$http', function ($http) {
+                            return $http.get('data/usuarios.json');
+                        }]
+                },
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'usuarios.html',
+                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                                $scope.usuariosRecords = usuarios.data;
+                            }]
+                    }
+                }
+            }).state('usuariosList', {
+                url: '/list',
+                parent: 'usuarios',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'usuarios.list.html'
+                    }
+                }
+            }).state('usuarioDetail', {
+                url: '/{usuarioId:int}/detail',
+                parent: 'usuarios',
+                param: {
+                    usuarioId: null
+                },
+                views: {
+                   
+                    'detailView': {
+                        templateUrl: basePath + 'usuarios.detail.html',
+                        controller: ['$scope', '$stateParams', function ($scope, $params) {
+                                $scope.currentUsuario = $scope.usuarioRecords[$params.usuarioId-1];
+                            }]
+                    }
+
+                }
+
+            });
+        }]);
+})(window.angular);
