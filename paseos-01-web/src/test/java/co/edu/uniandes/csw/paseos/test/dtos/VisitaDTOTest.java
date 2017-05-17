b9a2c7aa4c748694ef1bd9790e42707fd286f3fd
alejandro <alejandro@warmachine>
2017-05-17 01:40:10 -0500

    TestDTO visita

---
+++ paseos-01-web/src/test/java/co/edu/uniandes/csw/paseos/test/dtos/VisitaDTOTest.java
@@ -0,0 +1,68 @@
+package co.edu.uniandes.csw.paseos.test.dtos;
+
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 tf.venegas10.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import co.edu.uniandes.csw.paseos.dtos.VisitaDetailDTO;
+import co.edu.uniandes.csw.paseos.entities.FotoEntity;
+import co.edu.uniandes.csw.paseos.entities.VisitaEntity;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Venegas
+ */
+
+public class VisitaDTOTest {
+
+   
+    @Test
+    public void entityDtoentity() {
+        PodamFactory factory = new PodamFactoryImpl();
+        VisitaEntity visita = factory.manufacturePojo(VisitaEntity.class);
+        visita.setFotos(new ArrayList<FotoEntity>());
+        VisitaDetailDTO visitaDto = new VisitaDetailDTO(visita);
+        VisitaEntity entity = visitaDto.toEntity();
+
+        assertEquals(visita.getComentario(), entity.getComentario());
+        assertEquals(visita.getCalificacion(), entity.getCalificacion());
+    }
+
+    @Test
+    public void DtoEntityDto() {
+        PodamFactory factory = new PodamFactoryImpl();
+        VisitaDetailDTO visita = factory.manufacturePojo(VisitaDetailDTO.class);
+        VisitaEntity paseoE = visita.toEntity();
+        paseoE.setFotos(new ArrayList<FotoEntity>());
+        VisitaDetailDTO entity = new VisitaDetailDTO(paseoE);
+        
+
+        assertEquals(visita.getComentario(), entity.getComentario());
+        assertEquals(visita.getCalificacion(), entity.getCalificacion());
+    }
+}
